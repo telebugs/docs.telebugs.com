c7bd80b59e7228b4a4ed140ac437eac4a44c0154
Kyrylo Silin <silin@kyrylo.org>
2026-06-09 14:39:57 +0800

    build

--- public/searcher-c2a407aa.js
+++ public/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-535b263c.js',
+                path_to_root + 'searchindex-456ca6fa.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
